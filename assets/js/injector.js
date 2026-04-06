88906d84979b7c308e6d9ca20a03e90c932b39ed
directdestroyer1 <chenxi.lu@orange.fr>
2026-04-06 11:49:00 +0200

    Update injector.js and other

--- assets/js/injector.js
+++ assets/js/injector.js
@@ -4,7 +4,9 @@ console.log("Running injector...")
 function injectHead(text) { document.head.insertAdjacentHTML("afterbegin", text); }
 
 injectHead('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Manrope">');
-injectHead('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/directprogrammer1/mint-rd@main/assets/css/style.css">');
+injectHead('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/directprogrammer1/mint-rd/assets/css/style.css">');
+
+document.title = `Mint | ${document.title}`;
 
 function loadOverlay() {
     // load the overlay from overlay.html
