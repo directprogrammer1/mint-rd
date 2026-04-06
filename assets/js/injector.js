5ec244ef9cb0edc1ee242c9f91d32aa961a48585
directdestroyer1 <chenxi.lu@orange.fr>
2026-04-06 11:52:01 +0200

    Updated injector and overlay scripts

--- assets/js/injector.js
+++ assets/js/injector.js
@@ -8,6 +8,36 @@ injectHead('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/directprogr
 
 document.title = `Mint | ${document.title}`;
 
-function loadOverlay() {
+async function loadOverlay() {
     // load the overlay from overlay.html
+    try {
+        const res = await fetch("https://cdn.jsdelivr.net/gh/directprogrammer1/mint-rd/overlay.html");
+        
+        if (!res.ok) {
+            console.error("Failed to load overlay:", res.status, res.statusText);
+            return;
+        }
+
+        const html = await res.text();
+        
+        document.body.insertAdjacentHTML("beforeend", html);
+    } catch (e) {
+        console.error("Network error", e);
+        return;
+    }
 }
+
+async function runScript(src) {
+    try {
+        const res = await fetch(src);
+        const script = await res.text();
+        eval(script);
+    } catch (e) {
+        console.error("Network error", e);
+    }
+}
+
+runScript("https://cdn.jsdelivr.net/gh/directprogrammer1/mint-rd/assets/js/overlay.js");
+runScript("https://cdn.jsdelivr.net/gh/directprogrammer1/mint-rd/assets/js/tab.js");
+loadOverlay();
+// overlay should be hidden when not in fullscreen, otherwise can be shown with tab key/button
---
+++ assets/js/overlay.js
@@ -0,0 +1,32 @@
+// if fullscreen the style attribute will be shown, check every time with a mutationObserver
+
+function checkFullscreen() {
+    const isFullscreen = document.querySelectorAll('[class*="full-screen"]');
+    return isFullscreen.length > 0;
+
+    // wow very short function
+}
+
+function setFavicon() {
+    document.querySelectorAll('link[rel~="icon"]').forEach(el => el.remove());
+    injectHead('<link rel="icon" type="image/x-icon" href="https://cdn.jsdelivr.net/gh/directprogrammer1/mint-rd/assets/mint-logo.ico">');
+}
+
+const mutationObserver = new MutationObserver(() => {
+    mutationObserver.disconnect(); // overlay can be shown which would cause a crash
+
+    const overlay = document.querySelector(".overlay");
+
+    const isFullscreen = checkFullscreen();
+
+    overlay.classList.toggle("show", isFullscreen);
+
+    setFavicon();
+
+    mutationObserver.observe(document.body, { attributes: true, subtree: true });
+});
+
+
+mutationObserver.observe(document.body, { attributes: true, subtree: true });
+
+// closing function fow now
